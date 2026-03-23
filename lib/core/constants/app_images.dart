40c9fc0459b5060e11b797614285699abbabcf93
Moaz Ahmed <www.moazosama525@gmail.com>
2026-03-23 23:42:14 +0200

    style: Clean up whitespace and formatting in app_images.dart

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,6 +1,5 @@
 abstract class AppImages {
-
-  static const _imagesPath = "assets/images" ;
+  static const _imagesPath = "assets/images";
 
   static const String imageUserPhoto = "$_imagesPath/user_photo.png";
   static const String onboarding1 = "$_imagesPath/onboarding1.png";
@@ -16,14 +15,14 @@ abstract class AppImages {
   static const String onBoardingBgImage = "$_imagesPath/onboarding_bg.png";
   static const String homeBgImage = "$_imagesPath/home_bg.png";
   static const String authBg = "$_imagesPath/auth_bg.png";
-  static const String chatBg = "$_imagesPath/chat_bg.jpg"; 
+  static const String chatBg = "$_imagesPath/chat_bg.jpg";
   static const String userImage = "$_imagesPath/user_photo.png";
   static const String aiImage = "$_imagesPath/ropot.png";
   static const String maleImage = "$_imagesPath/male.svg";
   static const String femaleImage = "$_imagesPath/female.svg";
   static const String chestExerciseImage = "$_imagesPath/chest_exercise.jpg";
-  static const String exerciseCardImage = "$_imagesPath/exercise_card_image.png";
+  static const String exerciseCardImage =
+      "$_imagesPath/exercise_card_image.png";
   static const String foodBg = "$_imagesPath/food_bg.jpg";
   static const String forgetPasswordPG = "$_imagesPath/forgetPasswordPG.png";
-
 }
